4ceaf0d2bb339f1aa3866479c422f50131fc7a56
yuri.visser@verys.com <yuri.visser@verys.com>
2019-01-08 20:55:39 -0400

    added truffle-assertions and storage writing test from non owner/allowedSender

--- test/teststorage.js
+++ test/teststorage.js
@@ -1,4 +1,5 @@
-const LAFAssetStorage = artifacts.require("./LAFAssetStorage.sol")
+const truffleAssert = require('truffle-assertions')
+const LAFAssetStorage = artifacts.require('./LAFAssetStorage.sol')
 
 contract("LAFAssetStorage", accounts => {
     var assetStorageInstance
@@ -7,20 +8,53 @@ contract("LAFAssetStorage", accounts => {
         assetStorageInstance = await LAFAssetStorage.new({ from: accounts[0] })
 
         // console.log('accounts[0]', accounts[0])
+        // console.log('accounts[1]', accounts[1])
 
         await assetStorageInstance.setAllowedSender(accounts[1], { from: accounts[0] })
         let allowedSender = await assetStorageInstance.allowedSender()
-        // console.log('allowedSender (accounts[1])', allowedSender)
+        // console.log('allowedSender', allowedSender)
     })
 
-    it("...storeUint256 writable to owner & allowedSender only", async () => {
-        await assetStorageInstance.storeUint256(web3.utils.keccak256("uint256_key_owner_sender_test"), 99, { from: accounts[9] })
-        assert.ok(true)
+    it("...storeUint256 fails if not from owner or allowedSender", async () => {
+        await truffleAssert.fails(
+            assetStorageInstance.storeUint256(web3.utils.keccak256("uint256_key_owner_sender_test"), 99, { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
     })
     
-    it("...storedString writable to owner & allowedSender only", async () => {
-        await assetStorageInstance.storeString(web3.utils.keccak256("string_key_owner_sender_test"), "Hello storage", { from: accounts[9] })
-        assert.ok(true)
+    it("...storedString fails if not from owner or allowedSender", async () => {
+        await truffleAssert.fails(
+            assetStorageInstance.storeString(web3.utils.keccak256("string_key_owner_sender_test"), "Hello storage", { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
+    })
+
+    it("...storeAddress fails if not from owner or allowedSender", async () => {
+        await truffleAssert.fails(
+            assetStorageInstance.storeAddress(web3.utils.keccak256("address_key_owner_sender_test"), accounts[7], { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
+    })
+
+    it("...storeBytes if not from owner or allowedSender", async () => {
+        await truffleAssert.fails(
+            assetStorageInstance.storeBytes(web3.utils.keccak256("bytes_key_owner_sender_test"), web3.utils.hexToBytes(web3.utils.toHex("Byte me!")), { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
+    })
+
+    it("...storeBool if not from owner or allowedSender", async () => {
+        await truffleAssert.fails(
+            assetStorageInstance.storeBool(web3.utils.keccak256("bool_key_owner_sender_test"), false, { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
+    })
+
+    it("...storeInt256 if not from owner or allowedSender", async () => {
+        await truffleAssert.fails(
+            assetStorageInstance.storeInt256(web3.utils.keccak256("int256_key_owner_sender_test"), -99, { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
     })
 
     it("...uint256 write/read verifies (10x)", async () => {
