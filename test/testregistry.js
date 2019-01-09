6a476ac7a3a1b70917adaa1268b8054ce50780ab
yuri.visser@verys.com <yuri.visser@verys.com>
2019-01-08 22:26:07 -0400

    wip on asset flow. posted -> potential match -> match confirmed

--- test/testregistry.js
+++ test/testregistry.js
@@ -1,8 +1,12 @@
+const truffleAssert = require('truffle-assertions')
+
 const LAFAssetRegistry = artifacts.require("./LAFAssetRegistry.sol")
 const LAFAssetStorage = artifacts.require("./LAFAssetStorage.sol")
 
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 addAssetHelperNoAwait = async (assetRegistryInstance, title, description, countryIso, stateProvince, city, account) => {
-    assetRegistryInstance.newLostAsset(
+    await assetRegistryInstance.newLostAsset(
         title,
         description,
         web3.utils.asciiToHex(countryIso),
@@ -13,8 +17,8 @@ addAssetHelperNoAwait = async (assetRegistryInstance, title, description, countr
 }
 
 contract("LAFAssetRegistry", accounts => {
-    let assetRegistryInstance
-    let assetStorageInstance
+    var assetRegistryInstance
+    var assetStorageInstance
 
     before(async function () {
         assetRegistryInstance = await LAFAssetRegistry.new({ from: accounts[0] })
@@ -22,8 +26,6 @@ contract("LAFAssetRegistry", accounts => {
 
         // console.log('assetRegistryInstance.address', assetRegistryInstance.address)
         // console.log('assetStorageInstance.address', assetStorageInstance.address)
-
-        assetRegistryInstance.enableRegistry()
     })
     
     it("...set allowed sender in storage addresses & verify", async () => {
@@ -43,11 +45,6 @@ contract("LAFAssetRegistry", accounts => {
         assert.ok(assetRegistryInstance._registryEnabled())
     })
 
-    // it("..set initial assset count", async () => {
-    //     await assetRegistryInstance.setInitialAssetCount( { from: accounts[0] } )
-    //     assert.ok(true)
-    // })
-
     it("...add asset, retrieve & verify fields", async () => {
         let titleStr = "Asset Title Placeholder"
         let descriptionStr = "This is some rando placeholder content added during testing"
@@ -57,18 +54,20 @@ contract("LAFAssetRegistry", accounts => {
 
         // let { logs } = await addAssetHelper(assetRegistryInstance, titleStr, descriptionStr, countryIso, stateProvince, city, accounts[0])
 
+        let rewardStr = web3.utils.toWei("1.2345", 'ether').toString()
+
         let { logs } =  await assetRegistryInstance.newLostAsset(
             titleStr,
             descriptionStr,
             web3.utils.asciiToHex(countryIso),
             web3.utils.asciiToHex(stateProvince),
             web3.utils.asciiToHex(city),
-            { from: accounts[0] }
+            { from: accounts[1], value: rewardStr }
         )
         
         // get assetId from added item's logs
         let { assetId } = logs.find(x => x.event === 'AssetStored').args;
-        console.log(assetId.toString())
+        // console.log(assetId.toString())
 
         let retrievedAsset = await assetRegistryInstance.getAsset(assetId);
         // console.log(retrievedAsset)
@@ -79,5 +78,48 @@ contract("LAFAssetRegistry", accounts => {
         assert.equal(web3.utils.hexToAscii(retrievedAsset.isoCountryCode), countryIso)
         assert.equal(web3.utils.hexToAscii(retrievedAsset.stateProvince), stateProvince)
         assert.equal(web3.utils.hexToAscii(retrievedAsset.city), city)
+        assert.equal(web3.utils.fromWei(retrievedAsset.reward, 'ether'), 1.2345)
     })
+
+    it("...calling matchConfirmed on item with status AssetStatus.Posted item fails", async () => {
+        await truffleAssert.fails(
+            assetRegistryInstance.matchConfirmed(0, "Starbuck on corner of 43rd and 6th. April 1, 2019", { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
+    })
+
+    it("...calling matchConfirmed as non-asset creator fails", async () => {
+        // first call potentialMatch on item to get it to AssetStatus.PotentialMatch status
+        await assetRegistryInstance.potentialMatch(0, { from: accounts[2]} )
+
+        await truffleAssert.fails(
+            assetRegistryInstance.matchConfirmed(0, "Starbuck on corner on Noth and South April 1, 2019", { from: accounts[9] }),
+            truffleAssert.ErrorType.REVERT
+        )
+    })
+
+    it("...calling matchConfirmed as asset creator succeeds", async () => {
+        let retrievedAsset = await assetRegistryInstance.getAsset(0);
+        await assetRegistryInstance.matchConfirmed(0, "Starbuck on corner on Noth and South April 1, 2019", { from: retrievedAsset.creator })
+        assert.ok(true)
+    })
+    
+
+    // TODO do bulk add, then verify count as well as no duplicates asset IDs etc (will likely require new depolyment?)
+    // it("...bulk asset add - verify assetIds", async () => {
+    //     for(let i = 1; i <= 50; i++) {
+    //         await addAssetHelperNoAwait(assetRegistryInstance, 'Title_' + i, 'Description_' + i, 'CAD', 'SP', 'City_' + i, accounts[0])
+    //     }
+
+    //     console.log('waiting 5 seconds...')
+
+    //     await delay(5000);
+
+    //     for(let j = 1; j <= 50; j++) {
+    //         let retrievedAsset = await assetRegistryInstance.getAsset(j);
+    //         console.log('retrievedAsset.title', retrievedAsset.title + ", " + j)
+    //         assert.equal(retrievedAsset.title, 'Title_' + j-1)
+    //         // addAssetHelperNoAwait(assetRegistryInstance, 'Title_' + j, 'Description_' + j, 'CAD', 'ON', 'City_' + j, accounts[0])
+    //     }
+    // })
 })
