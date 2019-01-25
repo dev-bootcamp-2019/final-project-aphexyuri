b135106a72e8effdc6820eadb51c26e221cf20ab
yuri.visser@verys.com <yuri.visser@verys.com>
2019-01-24 20:28:46 -0400

    create client/src/contracts dir in migrations 3_

--- migrations/3_copy_artifacts.js
+++ migrations/3_copy_artifacts.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 module.exports = async(deployer, network, accounts) => {
   console.log('Copying LAFAssetRegistry.json to client/src/contracts...')
 
+  fs.mkdir('client/src/contracts/', 0744, (err) => {})
+
   fs.copyFile('build/contracts/LAFAssetRegistry.json', 'client/src/contracts/LAFAssetRegistry.json', (err) => {
     if (err) throw err;
   })
